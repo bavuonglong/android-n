f6e57c0b9c97579bc85045af8185336b24f723e1
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-03-06 07:26:14 +0700

    Fading Bart by alpha property and duration

--- app/src/main/java/com/codeko/androidn/MainActivity.java
+++ app/src/main/java/com/codeko/androidn/MainActivity.java
@@ -2,6 +2,8 @@ package com.codeko.androidn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fade(View view) {
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+        bart.animate().alpha(0f).setDuration(2000);
+
     }
 }
